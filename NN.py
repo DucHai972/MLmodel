b14157d9190e872cf8707f7cfd1e5a8f9776272a
Nguyễn Đức Hải <110242130+DucHai972@users.noreply.github.com>
2023-05-03 21:06:09 +0700

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,11 +1,11 @@
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error as mse, mean_absolute_error as mae
+import pandas as pd
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dense, Activation
 from keras.optimizers import Adam
-from joblib import dump, load
+from sklearn.metrics import mean_squared_error as mse, mean_absolute_error as mae
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
 
 data9 = pd.read_excel('Dataset1/data_match9.xlsx', engine='openpyxl')
 data10 = pd.read_excel('Dataset1/data_match10.xlsx', engine='openpyxl')
@@ -21,6 +21,8 @@ y = new_data['value']
 X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=2/9, random_state=42)
 
+'''
+#Tìm tham số tối ưu và dump mô hình
 model = Sequential()
 model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
 model.add(Dense(32, activation='relu'))
@@ -29,8 +31,11 @@ model.add(Dense(1))
 model.compile(optimizer=Adam(lr=0.001), loss='mse')
 
 history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=32)
+'''
 
-dump(model, 'NN.joblib')
+
+model = load_model('NN.h5')
+
 
 y_pred = model.predict(X_test)
 mse_nn = mse(y_test, y_pred)
