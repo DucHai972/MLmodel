4897aa03b0bb865bd033f8cbfd049e9baedb374c
Nguyễn Đức Hải <110242130+DucHai972@users.noreply.github.com>
2023-05-06 14:02:37 +0700

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -1,3 +1,4 @@
+import joblib
 import pandas as pd
 import numpy as np
 from sklearn.svm import SVR
@@ -6,6 +7,7 @@ from sklearn.metrics import mean_squared_error as mse, mean_absolute_error as ma
 from scipy.stats import pearsonr
 from sklearn.model_selection import GridSearchCV
 from joblib import dump, load
+
 
 data9 = pd.read_excel('Dataset1/data_match9.xlsx', engine='openpyxl')
 data10 = pd.read_excel('Dataset1/data_match10.xlsx', engine='openpyxl')
@@ -22,37 +24,23 @@ X_train_val, X_test, y_train_val, y_test, z_train_val, z_test = train_test_split
 X_train, X_val, y_train, y_val, z_train, z_val = train_test_split(X_train_val, y_train_val, z_train_val, test_size=2/9, random_state=42)
 
 '''
-###Tìm ra tham số tối ưu và dump mô hình
+#########
+param_grid = {'C': [0.01, 0.1, 1, 10], 'gamma': ['scale', 'auto', 1, 0.1, 0.01]}
+grid_search = GridSearchCV(SVR(kernel='rbf'), param_grid, cv=5, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+print("Các tham số tốt nhất: ", grid_search.best_params_)
 
-# khởi tạo một danh sách các giá trị cho các tham số cần tối ưu
-param_grid = {
-    'C': [0.1, 1, 10, 100, 1000],
-    'gamma': [0.1, 0.01, 0.001, 0.0001],
-    'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
-}
-
-# khởi tạo mô hình SVM
-svm_model = SVR()
-
-# sử dụng Grid Search để tìm kiếm các tham số tối ưu
-grid_search = GridSearchCV(svm_model, param_grid, cv=5, n_jobs=-1)
-grid_search.fit(X_train_val, y_train_val)
-
-# lấy ra các tham số tối ưu
-best_params = grid_search.best_params_
-print('Best params: ', best_params)
-
-# huấn luyện lại mô hình SVM với các tham số tối ưu
-svm_model = SVR(**best_params).fit(X_train, y_train)
+print('Training .... ')
+#Train mô hình và dump
+svm_model = SVR(kernel='rbf', C=grid_search.best_params_['C'], gamma=grid_search.best_params_['gamma']).fit(X_train, y_train)
+y_pred = svm_model.predict(X_train)
 '''
 
-#Train mô hình và dump
-svm_model = SVR(kernel='rbf', C=1e3, gamma=0.1).fit(X_train, y_train)
+print("Training .... ")
 
-dump(svm_model, 'SVM.joblib')
+svm_model = joblib.load('SVM.joblib')
 
-
-
+print('Evaluating... ')
 y_pred = svm_model.predict(X_train)
 print('Đánh giá training set (SVM): ')
 print('R2 score:', r2_score(y_train, y_pred))
